a5dcf3f3f92c4f2a0f8ae8d14d792e534d6b91b1
MavCode <161254271+MavCodeAI@users.noreply.github.com>
2025-04-10 17:56:58 +0430

    Delete src directory

--- src/pages/Index.tsx
+++
@@ -1,7 +0,0 @@
-import PromptImprover from "@/components/PromptImprover";
-const Index = () => {
-  return <div className="min-h-screen bg-gradient-to-b from-background to-brand-50/50 py-12 bg-zinc-500">
-      <PromptImprover />
-    </div>;
-};
-export default Index;
